7e5847dbb0df611e4cd41b56499f3ea6a03de32d
Calvin Bagby <cbagby33@gmail.com>
2026-03-09 01:31:13 -0400

    add state to clock componpent for session time to correspond with time set by controller

--- src/component/Clock.js
+++ src/component/Clock.js
@@ -3,19 +3,17 @@ import React from 'react';
 import TimeController from './TimeController'
 import Controls from './Controls'
 
-// Font Awesome dependencies
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-
-library.add(fas, fab) // add library of icons 
 class Clock extends React.Component {
   constructor(props){
     super(props)
     this.state ={
-      time: '25:00'
+      time: 1,
+      currentTimeType: 'Session'
     }
+    this.setTime = this.setTime.bind(this)
+  }
+  setTime(){
+    return this.state.time+':00'
   }
   render(){
     return(
@@ -23,9 +21,12 @@ class Clock extends React.Component {
         <div id="title">Interval clock</div>
         <div id="time-settings">
           <TimeController title="Break Time" time="5" />
-          <TimeController title="Session Time" time="25" />
+          <TimeController title="Session Time" time={this.state.time} />
         </div>
-        <div id="timer">{this.state.time}</div>
+        <div id="timer">
+          <p>{this.state.currentTimeType}</p>
+          <div>{this.setTime()}</div>
+        </div>
         <Controls />
       </div>
     )
