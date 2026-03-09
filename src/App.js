4ac5349ec7528f5e1e556cee171545cecdeb99d5
Calvin Bagby <cbagby33@gmail.com>
2026-03-08 23:07:19 -0400

    created initial structure for clock and functional elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+
+// React dependencies
+import React from 'react';
+
+// Font Awesome dependencies
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, fab) // add library of icons 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="title">Interval clock</div>
+      <div id="time-choices">
+        <div className="time-choice">
+          <div>Break Time</div>
+          <div className="time-setter"><FontAwesomeIcon icon="fa-solid fa-minus" />5<FontAwesomeIcon icon="fa-solid fa-plus" /></div>
+        </div>
+        <div className="time-choice">
+          <div>Session Time</div>
+          <div className="time-setter"><FontAwesomeIcon icon="fa-solid fa-minus" />25<FontAwesomeIcon icon="fa-solid fa-plus" /></div>
+        </div>
+      </div>
+      <div id="timer">25:00</div>
+      <div id="controls"><FontAwesomeIcon icon="fa-solid fa-play" /><FontAwesomeIcon icon="fa-solid fa-pause" /><FontAwesomeIcon icon="fa-solid fa-arrow-rotate-left" /></div>
+      
     </div>
   );
 }
